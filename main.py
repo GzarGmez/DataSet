e5b55383c77ff9659feea914aa41acc8638bcd36
GzarGmez <97687278+GzarGmez@users.noreply.github.com>
2024-04-19 00:23:54 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import pandas as pd
+import time
+
 
 # Carga el dataSet desde un archivo CSV
 data = pd.read_csv('D:\Phyton\DataSet\Spotify_final_dataset.csv')
 
-# Exploración inicial del dataSet
-print("Número de filas y columnas:", data.shape)
-print("\nPrimeras filas del dataSet:")
-print(data.head())
-print("\nInformación sobre el dataSet:")
-print(data.info())
-print("\nEstadísticas descriptivas del dataSet:")
-print(data.describe())
-print("\nNombres de las columnas del dataSet:")
-print(data.columns)
+# Define las estrategias y el número de núcleos a utilizar
+estrategias = ['estrategia_1', 'estrategia_2']
+nucleos = [2, 4, 6]
 
+# Definir las operaciones a realizar
+def calcular_estadisticas():
+    return data.describe()
+
+def limpiar_datos():
+    return data.drop_duplicates().dropna()
+
+# Bucle para iterar sobre las estrategias y el número de núcleos
+for estrategia in estrategias:
+    for n in nucleos:
+        start_time = time.time()  # Tiempo inicial
+        # Aquí realizamos las operaciones con la estrategia y número de núcleos actuales
+        # Por ejemplo, calculamos estadísticas descriptivas
+        if estrategia == 'estrategia_1':
+            resultado = calcular_estadisticas()
+        elif estrategia == 'estrategia_2':
+            resultado = limpiar_datos()
+        # Calcula el tiempo de ejecución
+        elapsed_time = time.time() - start_time
+        # Imprime los resultados y el tiempo de ejecución
+        print(f"\nEstrategia: {estrategia}, Núcleos: {n}")
+        print("Resultado de la operación:")
+        print(resultado)
+        print(f"Tiempo de ejecución: {elapsed_time} segundos")
+
+
+# Calcula la media de la columna 'Peak Streams'
+media_peak_streams = data['Peak Streams'].mean()
+
+# Imprime la media de 'Peak Streams'
+print(f"Media de 'Peak Streams': {media_peak_streams}")
